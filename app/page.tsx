eb19f8ddd2e594d2c2d1c59af8f1fced3bcd7500
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2025-09-18 21:41:41 -0400

    Add text to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 
 
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      {/* hero section */}
+      <section className="flex flex-col justify-center items-center md:justify-start md:items-start">
+        <div>
+          <h1>We're more than an answering service</h1>
+          <p>Answering Legal has everything you need to make sure your firm never misses another opportunity.</p>
+        </div>
+        <div className="flex items-center order-1 md:order-none">
+          <p>Excellent 4.84</p>
+          <div>
+            {/* stars */}
+            <p>based on 230 reviews</p>
+          </div>
+        </div>
+        <Link href="/pricing">See our pricing</Link>
+      </section>
+    </main>
   )
 }
